6136debc01e378cb3919b77d621159b4edd5bc44
le2yunji <162525231+le2yunji@users.noreply.github.com>
2025-01-14 21:23:05 +0900

    Update House.js

--- src/House.js
+++ src/House.js
@@ -1,4 +1,4 @@
-import { Stuff } from './Stuff';
+import * as THREE from 'three';
 
 export class House {
 	constructor(info) {
@@ -6,19 +6,33 @@ export class House {
 		this.y = info.y;
 		this.z = info.z;
 
+		this.rotationX = info.rotationX || 0; 
+		this.rotationY = info.rotationY || 0; 
+		this.rotationZ = info.rotationZ || 0;
+
 		this.visible = false; 
 
 		info.gltfLoader.load(
 			info.modelSrc,
 			glb => {
+
+				glb.scene.traverse(child => {
+					if (child.isMesh) {
+						child.castShadow = true;
+					}
+				});
+				
 				this.modelMesh = glb.scene.children[0];
 				this.modelMesh.castShadow = true;
 				this.modelMesh.position.set(this.x, this.y, this.z);
+				
+				// 회전 설정
 				// this.modelMesh.rotation.set(
-                //     this.rotationX,
-                //     this.rotationY,
-                //     this.rotationZ
-                // )
+				// 	THREE.MathUtils.degToRad(this.rotationX),
+				// 	THREE.MathUtils.degToRad(this.rotationY),
+				// 	THREE.MathUtils.degToRad(this.rotationZ)
+				// );
+
 				info.scene.add(this.modelMesh);
 				
 			}
